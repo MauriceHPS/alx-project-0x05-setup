59cac27b47e0771d336f4b80f1e5c11f890bfea6
MauriceHPS <maurice@headpoint.info>
2026-01-23 12:50:26 +0300

    Added some state

---
+++ alx-project-0x08/pages/index.tsx
@@ -0,0 +1,53 @@
+import ImageCard from "@/components/common/ImageCard";
+import Layout from "@/components/layouts/Layout";
+import { ImageProps } from "next/image";
+
+import React, { useState } from "react";
+
+const Home: React.FC = () => {
+  const [prompt, setPrompt] = useState<string>("");
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const [generatedImages, setGeneratedImages] = useState<ImageProps>([]);
+
+  const handleGenerateImage = async () => {
+    console.log("Generating Images.");
+  };
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+        <div className="flex flex-col items-center">
+          <h1 className="text-4xl font-bold text-gray-950/30 mb-2">
+            Image Generation App
+          </h1>
+          <p className="text-lg text-gray-700 mb-4">
+            Generating stunning images based on your prompts!
+          </p>
+          <div className="w-full max-w-md">
+            <input
+              type="text"
+              placeholder="Enter your prompt here....."
+              className="w-full p-3 border border-gray-300 text-black rounded-lg mb-4"
+            />
+            <button
+              onClick={handleGenerateImage}
+              className="w-full p-3 bg-blue-600 text-white rounded-lg
+               hover:bg-blue-700 transition duration-200"
+            >
+              {/* {isLoading? "Loading..." : "Generate Image}" */}
+              Generate Image
+            </button>
+          </div>
+          {imageUrl && (
+            <ImageCard
+              action={() => setImageUrl(imageUrl)}
+              imageUrl={imageUrl}
+              prompt={prompt}
+            />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
