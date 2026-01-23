5ba55477450d4b1094fda3f4327d174845cfd795
MauriceHPS <maurice@headpoint.info>
2026-01-23 13:07:07 +0300

    Linked the project with the CHATGPT-4 API for generating images

---
+++ alx-project-0x09/components/layouts/Layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { ReactComponentProps } from "@/interfaces";
+
+const Layout: React.FC<ReactComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
